9ec1e2c30618eb049f2b53a03214fd4602098159
Saleem Abdulrasool <compnerd@compnerd.org>
2018-12-20 18:39:35 -0800

    caches: update caches for Foundation
    Add CFLAGS for swift code to allow compilation

--- cmake/caches/swift-corelibs-foundation-Windows-x86_64.cmake
+++ cmake/caches/swift-corelibs-foundation-Windows-x86_64.cmake
@@ -4,6 +4,15 @@ set(CMAKE_C_COMPILER_TARGET x86_64-unknown-windows-msvc CACHE STRING "")
 # TODO(compnerd) figure out how to break the cycle between XCTest and Foundation
 set(ENABLE_TESTING NO CACHE BOOL "")
 
+# NOTE(compnerd) we need to override the include order for the Windows SDK to
+# accomodate the conflicts with the compiler resource dir and promote the SDK to
+# system level.  The include ordering is important to the construction of the
+# modules for building libdispatch.  Note that `-internal-isystem` is not a
+# driver level flag, so we need to pass that through the C compiler frontend
+# (which is guaranteed to be clang).  So, we end up using `-Xcc` to pass a flag
+# to the driver that passes through the C driver to the compiler (`-Xclang`).
+set(CMAKE_SWIFT_FLAGS -Xcc -Xclang -Xcc -ivfsoverlay -Xcc -Xclang -Xcc ${CMAKE_BINARY_DIR}/windows-sdk-vfs-overlay.yaml -Xcc -Xclang -Xcc -internal-isystem -Xcc -Xclang -Xcc $ENV{VCToolsInstallDir}/include -Xcc -Xclang -Xcc -internal-isystem -Xcc -Xclang -Xcc $ENV{UniversalCRTSdkDir}/Include/$ENV{UCRTVersion}/ucrt -Xcc -Xclang -Xcc -internal-isystem -Xcc -Xclang -Xcc $ENV{UniversalCRTSdkDir}/Include/$ENV{UCRTVersion}/um -Xcc -Xclang -Xcc -internal-isystem -Xcc -Xclang -Xcc $ENV{UniversalCRTSdkDir}/Include/$ENV{UCRTVersion}/shared CACHE STRING "")
+
 # --- libraries to use ---
 
 # TODO(compnerd) build these from source
