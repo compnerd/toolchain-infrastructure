b711db8e6e47ba46410c1e9519303b5d84395688
Saleem Abdulrasool <compnerd@compnerd.org>
2018-12-20 17:24:20 -0800

    caches: update location for ICU, CURL, libxml2
    These libraries are needed to build the standard library and Foundation.
    Update the paths for these.

--- cmake/caches/swift-corelibs-foundation-Windows-x86_64.cmake
+++ cmake/caches/swift-corelibs-foundation-Windows-x86_64.cmake
@@ -7,8 +7,16 @@ set(ENABLE_TESTING NO CACHE BOOL "")
 # --- libraries to use ---
 
 # TODO(compnerd) build these from source
-set(ICU_ROOT ${TOOLCHAIN_SOURCE_DIR}/build/icu-59.1-vs2017 CACHE STRING "")
-set(LIBXML2_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/libxml2-2.9.2/lib/libxml2.dll.a CACHE STRING "")
+set(ICU_INCLUDE_DIR ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/icu/include CACHE STRING "")
+set(ICU_I18N_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/icu/lib/icuin.lib CACHE STRING "")
+set(ICU_UC_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/icu/lib/icuuc.lib CACHE STRING "")
+set(ICU_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/icu/lib/icuin.lib;${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/icu/lib/icuuc.lib CACHE STRING "")
+
+set(LIBXML2_INCLUDE_DIR ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/libxml2/include CACHE STRING "")
+set(LIBXML2_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/libxml2/lib/libxml2.lib CACHE STRING "")
+
+set(CURL_INCLUDE_DIR ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/curl/include CACHE STRING "")
+set(CURL_LIBRARY ${TOOLCHAIN_SOURCE_DIR}/build/thirdparty/curl/lib/libcurl.lib CACHE STRING "")
 
 set(CMAKE_FIND_ROOT_PATH "/var/empty" CACHE STRING "")
 
